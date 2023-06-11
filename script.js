b5a62e305a7d31fd5e89e0bded9862bc5e6a3185
fateme kholousi <fatemekholousizaer@gmail.com>
2023-06-11 20:33:44 +0330

    feat : first version of WJSN play list created!

---
+++ script.js
@@ -0,0 +1,89 @@
+const stopBtn = document.getElementById("stop");
+const currentTime = document.getElementById("current-time");
+const volume = document.getElementById("volume");
+const playBtn = document.getElementById("play");
+const pauseBtn = document.getElementById("pause");
+const musicContainer = document.getElementById("music-container");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+
+// Songs
+const songs = ["pantomime", "save-me-save-you", "secret", "unnatural"];
+
+let songIndex = 3;
+
+// initially load song details
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+  audio.pause();
+}
+
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function updateProgress(event) {
+  const { duration, currentTime } = event.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(event){
+const width=this.clientWidth
+const clickX=event.offsetX
+const duration=audio.duration
+audio.currentTime=(clickX/width)*duration
+}
+
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+prevBtn.addEventListener("click", prevSong);
+
+nextBtn.addEventListener("click", nextSong);
+
+audio.addEventListener("timeupdate", updateProgress);
+
+progressContainer.addEventListener('click',setProgress)
